3c3989f4724ead3fd8d249c003cf88433add0f95
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-03-13 17:49:02 +0100

    [add] AccountStore.delete()

--- src/accounts.ts
+++ src/accounts.ts
@@ -120,24 +120,24 @@ export class AccountStore extends EventEmitter<Record<LogLevel, any[]>> implemen
      * @param user User information - username, password, root
      * @param skipChecks Whether to skip password strength checks (used for default admin account creation only)
      */
-    public async create(user: z.infer<typeof this.CreateParams>, skipChecks = false) {            
+    public async create(user: z.infer<typeof this.CreateParams>, skipChecks = false) {
         try {
             
-            this.emit('notice', `AccountStore.create() call made | name:${user.name}, root:${user.root}`)
+            this.emit('notice', `AccountStore.create() call made | user:${user.name}, root:${user.root}`)
 
             // Check if name is taken
-            if (await this.exists(user.name)) return 'err_name_taken'
+            if (await this.exists(user.name)) return 'ERR_NAME_TAKEN'
 
             // Check against username and password security requirements
             if (!skipChecks) {
-                if (!this.CreateParams.safeParse(user).success)                                return 'err_bad_entry'
-                if (this.settings.user.minUsernameLength          > user.name.length)          return 'err_name_too_short'
-                if (this.settings.user.maxUsernameLength          < user.name.length)          return 'err_name_too_long'
-                if (this.settings.password.minPasswordLength      > user.pass.length)          return 'err_pass_too_short'
-                if (this.settings.password.useNumbers             && !/[0-9]/.test(user.pass)) return 'err_pass_no_nums'
-                if (this.settings.password.useBigAndLittleSymbols && !/[A-Z]/.test(user.pass)) return 'err_pass_no_big_chars'
-                if (this.settings.password.useBigAndLittleSymbols && !/[a-z]/.test(user.pass)) return 'err_pass_no_small_chars'
-                if (this.settings.password.useSpecialCharacters   && !/\W/   .test(user.pass)) return 'err_pass_no_special_chars'
+                if (!this.CreateParams.safeParse(user).success)                                return 'ERR_BAD_ENTRY'
+                if (this.settings.user.minUsernameLength          > user.name.length)          return 'ERR_NAME_TOO_SHORT'
+                if (this.settings.user.maxUsernameLength          < user.name.length)          return 'ERR_NAME_TOO_LONG'
+                if (this.settings.password.minPasswordLength      > user.pass.length)          return 'ERR_PASS_TOO_SHORT'
+                if (this.settings.password.useNumbers             && !/[0-9]/.test(user.pass)) return 'ERR_PASS_NO_NUMS'
+                if (this.settings.password.useBigAndLittleSymbols && !/[A-Z]/.test(user.pass)) return 'ERR_PASS_NO_BIG_CHARS'
+                if (this.settings.password.useBigAndLittleSymbols && !/[a-z]/.test(user.pass)) return 'ERR_PASS_NO_SMALL_CHARS'
+                if (this.settings.password.useSpecialCharacters   && !/\W/   .test(user.pass)) return 'ERR_PASS_NO_SPECIAL_CHARS'
             }
             
             const pwdHash = await bcrypt.hash(user.pass, this.settings.password.saltRounds)
@@ -153,11 +153,43 @@ export class AccountStore extends EventEmitter<Record<LogLevel, any[]>> implemen
                 lastLoginISO: 'never'
             })
 
-            this.emit('notice', `AccountStore.create() call succeeded | name:${user.name}, root:${user.root}, uuid:${userID}`)
+            this.emit('notice', `AccountStore.create() call succeeded | user:${user.name}, root:${user.root}, uuid:${userID}`)
 
         } 
         catch (error) {
             this.emit('error', `Account.create() error:`, error)
+            return error as Error
+        }
+    }
+
+    /**
+     * Deletes the user account specified by tne "name".  
+     * Returns an error if any execution errors appear, if the user isn't found
+     * or if is the last root in the database.
+     * @param name Username of the account to delete.
+     * @returns An error, if any happened.
+     */
+    public async delete(name: string) {
+        try {
+
+            this.emit('notice', `AccountStore.delete() call made | user:${name}`)
+
+            // Check if user exists
+            if (await this.exists(name) === false) return 'ERR_USER_NOT_FOUND'
+
+            // Check if the username doesn't belong to the last admin account
+            // and prevent cases where there wouldn't be any admin accounts at all.
+            const [listError, userList] = await this.listAccountEntries()
+            if (listError) return listError
+            const admins = userList.filter(x => x.root)
+            if (admins.length === 1 && admins[0]!.name === name) return 'ERR_CANT_DEL_LAST_ADMIN'
+
+            this.slAccounts.del(name)
+
+            this.emit('notice', `AccountStore.delete() call successful | user:${name}`)
+            
+        } 
+        catch (error) {
             return error as Error
         }
     }
